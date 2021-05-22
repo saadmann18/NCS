d153e77cf44cc9095185ab9cec0750010316df2e
saadmann18 <saadmann18@gmail.com>
2021-05-22 21:31:30 +0200

    updated with train

--- ae.py
+++ ae.py
@@ -2,6 +2,8 @@ from tensorflow.keras import Model
 from tensorflow.keras.layers import Input, Conv2D, ReLU, BatchNormalization, \
     Flatten, Dense, Reshape, Conv2DTranspose, Activation
 from tensorflow.keras import backend as K
+from tensorflow.keras.optimizers import Adam
+from tensorflow.keras.losses import MeanSquaredError
 import numpy as np
 
 class Autoencoder:
@@ -27,16 +29,37 @@ class Autoencoder:
         #private attributes
         self._num_conv_layers = len(conv_filters)
         self._shape_before_bottleneck = None
+        self._model_input = None
 
         self._build()
+
     def summary(self):
         self.encoder.summary()
         self.decoder.summary()
+        self.model.summary()
+
+    def compile(self, learning_rate=0.0001):
+        optimizer = Adam(learning_rate=learning_rate)
+        mse_loss = MeanSquaredError()
+        self.model.compile(optimizer=optimizer, loss=mse_loss)
+
+    def train(self, x_train, batch_size, num_epochs):
+        self.model.fit(x_train,
+                       x_train,
+                       batch_size = batch_size,
+                       epochs=num_epochs,
+                       shuffle=True)
+
 
     def _build(self):
         self._build_encoder()
         self._build_decoder()
-        #self._build_autoencoder()
+        self._build_autoencoder()
+
+    def _build_autoencoder(self):
+        model_input = self._model_input
+        model_output = self.decoder(self.encoder(model_input))
+        self.model = Model(model_input, model_output, name="autoencoder")
 
     def _build_decoder(self):
         decoder_input = self._add_decoder_input() #data comes from bottleneck
@@ -96,6 +119,7 @@ class Autoencoder:
         encoder_input = self._add_encoder_input()
         conv_layers = self._add_conv_layers(encoder_input)
         bottleneck = self._add_bottleneck(conv_layers)
+        self._model_input = encoder_input
         self.encoder = Model(encoder_input, bottleneck, name="encoder")
 
     def _add_encoder_input(self):
